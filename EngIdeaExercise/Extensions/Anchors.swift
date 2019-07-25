19d0008a261ac3db75cbe48db0e1966071d66c59
MSQUARDIAN <warbeast456@gmail.com>
2019-07-25 14:27:42 +0300

    added photoLoading functionality to GalleryViewController

---
+++ EngIdeaExercise/Extensions/Anchors.swift
@@ -0,0 +1,40 @@
+//
+//  Anchors.swift
+//  EngIdeaExercise
+//
+//  Created by MSQUARDIAN on 7/25/19.
+//  Copyright © 2019 MSQUARDIAN. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchorTo(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsTo(top: top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsTo(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: topConstant).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: leftConstant).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -rightConstant).isActive = true
+        }
+        
+    }
+    
+}
